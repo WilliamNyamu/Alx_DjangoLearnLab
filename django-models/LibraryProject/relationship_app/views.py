0a6f179435f459bd288048a8f3cb24e48dd6b2fd
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-23 14:17:58 +0300

    user .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from .models import Library
+from django.contrib.auth import login
 from django.http import request
 from django.shortcuts import render
 from .models import Author, Book, Librarian
@@ -21,4 +22,3 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = "relationship_app/register.html"
     success_url = reverse_lazy('login')
-    
