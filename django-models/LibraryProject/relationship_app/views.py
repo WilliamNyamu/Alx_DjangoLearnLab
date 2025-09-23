b65079f38201132d76fa02f2ff42aea762a328d0
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-23 12:42:41 +0300

    Views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
+from django.http import request
 from django.shortcuts import render
-
+from .models import Author, Book, Library, Librarian
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.txt', {'books': books})
