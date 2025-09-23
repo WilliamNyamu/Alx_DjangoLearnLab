50a728ab087e0ea32b455eed0cd8db83872ea289
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-23 12:19:08 +0300

    Listing all the books in a library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,11 @@
-from .models import Author, Book
+from .models import Author, Book, Library
 
 def queries():
     # Query all books by a specific author
     author = Book.objects.get(author = "Prof Kibugi")
     books_by_author = Book.objects.filter(author = author)
+
+    # List all the books in a library
+    lib_name = Library.objects.get(name = "LibraryProject")
+    for book in lib_name.books.all():
+        print(book)
