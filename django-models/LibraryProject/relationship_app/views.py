88fcff6ac4fe14b5946055b9d708ad9c5f8f19fa
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-23 13:05:59 +0300

    Corrected

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render
-from .models import Author, Book, Library, Librarian
+from .models import Author, Book, Librarian
+from.models import Library
 from django.views.generic import ListView, DetailView
 # Create your views here.
 
