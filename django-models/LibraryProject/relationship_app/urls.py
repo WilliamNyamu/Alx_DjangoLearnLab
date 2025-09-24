888d5a4efee3b13e64a16d9e6818594af40a3cbe
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-24 15:34:49 +0300

    4th question

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns= [
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
     path("admin_view/", views.admin_view, name="admin_view"),
     path("librarian_view/", views.librarian_view, name="librarian_view"),
     path("member_view/", views.member_view, name="member_view"),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,8 @@
 from .models import Library
 from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, permission_required
 from django.http import request
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Author, Book, Librarian
 from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
@@ -74,3 +74,25 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
+# Add book view
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    # logic to add book
+    return render(request, 'relationship_app/add_book.html')
+
+
+# Edit book view
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # logic to edit book
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+
+# Delete book view
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # logic to delete book
+    return redirect('list_books')
