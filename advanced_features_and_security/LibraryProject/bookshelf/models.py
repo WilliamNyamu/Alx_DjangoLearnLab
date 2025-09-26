e73fe62214302bc00d05891b56f143eb89b4df3f
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-26 16:54:54 +0300

    Base User Manager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 # Create your models here.
@@ -11,7 +11,48 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} ({self.publication_year})"
     
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user manager where email is unique identifier (or use username if you prefer).
+    This manager supports creating users with date_of_birth and profile_photo.
+    """
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(username, email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
-    # Adding custom fields
-    date_of_birth = models.DateField()
-    profile_photo = models.ImageField(upload_to="profile/")
+    """
+    Extends Django's AbstractUser. Adds date_of_birth and profile_photo.
+    """
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    def __str__(self):
+        return self.username
