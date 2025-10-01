251da4296fa307f1254c101b6ac00bcb5a0d72a7
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-01 23:55:56 +0300

    added urlpatterns for the api views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("books", views.ListView.as_view()),
-    path("book/<int:pk>/", views.DetailView.as_view()),
+    path("books/<int:pk>/", views.DetailView.as_view()),
+    path("books/create/", views.CreateView.as_view()),
+    path("books/update/", views.UpdateAPIView.as_view()),
+    path("books/delete/", views.DeleteView.as_view()),
 ]
