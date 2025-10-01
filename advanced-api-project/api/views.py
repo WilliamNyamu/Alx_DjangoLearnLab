834b605cdb1f039b73ec0a9925bc5af09dfc2fc4
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-01 23:48:15 +0300

    added views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 # Create your views here.
 
 class ListView(ListAPIView):
@@ -16,5 +16,10 @@ class CreateView(CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class UpdateView(UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-
+class DeleteView(DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
