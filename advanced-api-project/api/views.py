fc4303e4ca00e810b5d0fe982eb8fd78caabf92c
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-02 00:28:50 +0300

    repositioned the permissions import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,19 +1,20 @@
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django.shortcuts import render
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+
 # Create your views here.
 
 class ListView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class DetailView(RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CreateView(CreateAPIView):
     queryset = Book.objects.all()
