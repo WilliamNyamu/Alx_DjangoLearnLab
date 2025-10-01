e45f95a402b2d006b8abbe592037e2526e82b389
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-02 00:29:47 +0300

    rearranged permissions imports

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django.shortcuts import render
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
