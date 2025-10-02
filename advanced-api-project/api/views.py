6b9a2deb3b4cf5419370ad7b7d9985131d6053e3
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-02 12:22:59 +0300

    configured filter, search, and order in BookList view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics
+from rest_framework import filters
 from django_filters import rest_framework
 from django.shortcuts import render
 from .models import Book, Author
@@ -12,11 +13,14 @@ class ListView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [rest_framework.DjangoFilterBackend]
+    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = {
-        'title': ['icontains'],
-        # 'author_name': ['icontains']
+        'title': ['icontains'], # allows for partial matches of the title field
+        # 'author_name': ['icontains'] # couldn't work because author_name is not a model field
     }
+    search_fields = ['title'] # search using the title field
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['-publication_year'] # the default ordering. -publication_year means from a reverse order: newest to oldest
 
 class DetailView(RetrieveAPIView):
     queryset = Book.objects.all()
