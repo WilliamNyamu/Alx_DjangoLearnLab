54044c2563f2c5ec8ff2ae6bfa390ede82ab952d
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-02 11:28:02 +0300

    refined the validate method in BookSerializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -6,13 +6,13 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ['id', 'title', 'publication_year', 'author']
     
-    def validate(self, attrs):
+    def validate(self, data):
         from datetime import datetime
         current_date = datetime.now()
         current_year = current_date.year
-        if attrs['publication_year'] > current_year:
+        if data['publication_year'] > current_year:
             raise serializers.ValidationError("Publication year cannot be in the future")
-        return None
+        return data
 
 
 
--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,9 +3,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 urlpatterns = [
-    path("books", views.ListView.as_view()),
+    path("books/", views.ListView.as_view()),
     path("books/<int:pk>/", views.DetailView.as_view()),
     path("books/create/", views.CreateView.as_view()),
     path("books/update/", views.UpdateAPIView.as_view()),
     path("books/delete/", views.DeleteView.as_view()),
+    path("api-auth/", include('rest_framework.urls'))
 ]
