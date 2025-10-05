21f664f2e0bd98e3c9b11584122f0918df730d8a
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-05 17:49:52 +0300

    urls updated

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("posts/", views.PostsView.as_view(), name="posts"),
     path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
-    path("posts/create/", views.PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post-edit"),
+    path("posts/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("posts/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-edit"),
     path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
     path("", views.index, name="index")
 ]
