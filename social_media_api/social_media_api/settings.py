81b5ecc9ae7da7a53a1ebc9a11f4315dedb874e2
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-12 22:52:49 +0300

    static file changes

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # (just copied from a repo)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
