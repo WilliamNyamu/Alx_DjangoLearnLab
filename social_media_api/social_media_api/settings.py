bda5248752fd445925b5f40136a0e0ba36a4befa
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-12 22:18:57 +0300

    security settings added

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -139,3 +139,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+# Security Settings
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
