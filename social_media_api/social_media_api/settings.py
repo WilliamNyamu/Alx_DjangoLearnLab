a658c71941dcf81a4bf9f7ebd41c53ff505f9d36
WilliamNyamu <williamnyamu08@gmail.com>
2025-10-12 22:53:33 +0300

    added static root

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # (just copied from a repo)
 
 # Default primary key field type
